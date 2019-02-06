1de26a4d1134ab76085edc33075e6a66fb0d1a74
Binod Raj Pandey <binod.pandey@logicabeans.com>
2019-02-06 18:54:51 +0545

    code refactor.

--- example/src/main/java/myapps/ProducerApps.java
+++ example/src/main/java/myapps/ProducerApps.java
@@ -3,30 +3,51 @@ package myapps;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ProducerApps
 {
-    public static void main(String[] args){
-        Properties properties=new Properties();
-        properties.put("bootstrap.servers","localhost:9092");
-        properties.put("acks","all");
-        properties.put("retries",0);
-        properties.put("key.serializer",
-                  "org.apache.kafka.common.serialization.StringSerializer");
+    public static Logger logger = LoggerFactory.getLogger(ProducerApps.class);
 
-//        properties.put("value.serializer",
-//                  "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer",
-                       "myapps.UserSerializer");
-        String topicName="streams-wordcount-output";
-        Producer<String,User> producer=new KafkaProducer<String, User>(properties);
-        for(char i='a';i<='z';i++){
-            System.out.println("record sent");
-            producer.send(new ProducerRecord<>(topicName,String.valueOf(i),new User(i,"name"+i,"address"+i)));
+    public static void main(String[] args)
+    {
+        Properties properties = new Properties();
+        String topicName = StringConstant.TOPIC_NAME;
 
+        try
+        {
+            InputStream input = new FileInputStream(StringConstant.PRODUCER_CONFIG_LOCATION);
+            properties.load(input);
+            Producer<String, User> producer = new KafkaProducer<>(properties);
+            User user;
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                logger.info("record is being sent");
+                user = new User(i, "name" + i, "address" + i);
+                try
+                {
+                    producer.send(new ProducerRecord<>(topicName, String.valueOf(i), user));
+
+                }
+                catch (Exception ex)
+                {
+                    logger.error("Could not send user: {}", user);
+                }
+
+            }
+            producer.close();
         }
-        producer.close();
+        catch (IOException ex)
+        {
+            logger.error(ex.getMessage());
+        }
+
     }
 }
